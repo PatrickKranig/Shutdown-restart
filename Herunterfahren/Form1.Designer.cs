c24e7a2fc84841c2efe03254ecc99cb2149e2a48
Patrick Kranig <Patrick.Kranig@live.de>
2019-01-21 10:33:32 +0100

    added the project and the source code

---
+++ Herunterfahren/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace Herunterfahren
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(110, 87);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(70, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(190, 128);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(177, 82);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Herunterfahren/neustarten abbrechen";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(322, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Bitte Zeit eingeben wann heruntergefahren werden soll:";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(190, 87);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(70, 20);
+            this.numericUpDown2.TabIndex = 3;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(130, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Std";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(214, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Min";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(157, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Uhrzeit (Timer)";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 25);
+            this.label5.TabIndex = 9;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.Location = new System.Drawing.Point(274, 218);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 31);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Be&enden";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(7, 227);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(135, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Copyright by P.Kranig 2019";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(10, 128);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(170, 38);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "PC herunterfahren nach Zeit...";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(89, 87);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(15, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "in";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(170, 112);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(25, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "und";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 172);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 38);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "PC neustarten nach Zeit...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 248);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Shutdown";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Herunterfahren/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+
+
+namespace Herunterfahren
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //label4.Text(System.Windows.Forms.Timer());
+        }
+
+  
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(System.Environment.SystemDirectory + "\\shutdown.exe", "-a");
+            //herunterfahren Label leeren
+            
+            label5.Text = "";
+            System.Media.SystemSounds.Beep.Play(); //Beep
+            MessageBox.Show("OK, das Herunterfahren/neustarten wurde beendet");          
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //https://www.youtube.com/watch?v=CVhTlx3dW5E
+            timer1.Start();
+            label4.Text = "es ist momentan "+DateTime.Now.ToLongTimeString()+" Uhr";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //https://www.youtube.com/watch?v=CVhTlx3dW5E
+            label4.Text = "es ist momentan " + DateTime.Now.ToLongTimeString() + " Uhr";
+            timer1.Start();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Deklarierung + Umkonvertieren + Initalisieren
+            string t_std = numericUpDown1.Value.ToString();
+            string t_min = numericUpDown2.Value.ToString();
+            int t_Std = int.Parse(t_std);
+            int t_Min = int.Parse(t_min);
+            int t = (t_Std * 60 * 60) + (t_Min * 60); //sekunden zusammenfassen
+            int AbfrageMin = 0;
+            int AbfrageStd = 0;
+            int StdHöher = 0;
+
+            if (t_Min > 60) //Abfangen wenn über 60min
+            {
+                System.Media.SystemSounds.Beep.Play(); //Beep
+                StdHöher = t_Min / 60;
+                AbfrageMin = (t_Min - 60) / StdHöher;
+                AbfrageStd = (t_Std + StdHöher);
+
+
+                MessageBox.Show("BEACHTE! 60min = 1 Std | "+t_min+" min = "+ AbfrageStd+" std "+AbfrageMin+" min ... Bitte nochmal eingeben!", "Fehler bei der Minuten Eingabe");
+                numericUpDown2.Value = AbfrageMin;
+                numericUpDown1.Value = AbfrageStd;
+                return;
+            }
+
+            System.Media.SystemSounds.Beep.Play(); //Beep
+
+            int sekunden = (DateTime.Now.Hour * 3600 + DateTime.Now.Minute * 60 + DateTime.Now.Second + t);
+            //aktuelle zeit in sekunden + die eingegebene sekunden zeit!
+
+            TimeSpan time = TimeSpan.FromSeconds(sekunden);
+            label5.Text = "der PC fährt herunter um " + time.ToString(@"hh\:mm\:ss");
+
+            //    label4.Text = t.ToString();
+            //     MessageBox.Show(t); //schmeisse zum test eine MessageBox (nur strings!)
+
+
+           
+            //Deklarierung + Umkonvertieren + Initalisieren der Berechnung
+            int t2 = (t_Std * 60 * 60) + (t_Min * 60); //sekunden 
+           //Deklaration einer neuen Klasse für eine Abfrage Box mit Ja/NEIN
+            DialogResult dialogResult = MessageBox.Show("Sind sie sicher?", "Wirklich?", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                //wennn ja
+                //Deklarierung + Umkonvertieren + Initalisieren
+                t2 = (t_Std * 60 * 60) + (t_Min * 60); //sekunden zusammenfassen
+
+                //    label4.Text = t.ToString();
+                //     MessageBox.Show(t); //schmeisse zum test eine MessageBox (nur strings!)
+
+                System.Media.SystemSounds.Beep.Play(); //Beep
+
+                System.Diagnostics.Process.Start(System.Environment.SystemDirectory + "\\shutdown.exe", "-s -f -t " + t2);
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                //wenn nein
+                label5.Text = "";
+                //Setze die EingabeBoxen wieder auf 0
+                numericUpDown1.Value = 0;
+                numericUpDown2.Value = 0;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+          label5.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) //Keys.A definierrt Taste A
+            {
+                button4.PerformClick(); //-> Button betätigen
+            }
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+           //Wenn Enter, dann führe aus
+            AcceptButton = button4;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            //Wenn Enter, dann führe aus
+            AcceptButton = button4;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Deklarierung + Umkonvertieren + Initalisieren
+            string t_std = numericUpDown1.Value.ToString();
+            string t_min = numericUpDown2.Value.ToString();
+            int t_Std = int.Parse(t_std);
+            int t_Min = int.Parse(t_min);
+            int t = (t_Std * 60 * 60) + (t_Min * 60); //sekunden zusammenfassen
+            int AbfrageMin = 0;
+            int AbfrageStd = 0;
+            int StdHöher = 0;
+
+            if (t_Min > 60) //Abfangen wenn über 60min
+            {
+                System.Media.SystemSounds.Beep.Play(); //Beep
+                StdHöher = t_Min / 60;
+                AbfrageMin = (t_Min - 60) / StdHöher;
+                AbfrageStd = (t_Std + StdHöher);
+
+
+                MessageBox.Show("BEACHTE! 60min = 1 Std | " + t_min + " min = " + AbfrageStd + " std " + AbfrageMin + " min ... Bitte nochmal eingeben!", "Fehler bei der Minuten Eingabe");
+                numericUpDown2.Value = AbfrageMin;
+                numericUpDown1.Value = AbfrageStd;
+                return;
+            }
+
+            System.Media.SystemSounds.Beep.Play(); //Beep
+
+            int sekunden = (DateTime.Now.Hour * 3600 + DateTime.Now.Minute * 60 + DateTime.Now.Second + t);
+            //aktuelle zeit in sekunden + die eingegebene sekunden zeit!
+
+            TimeSpan time = TimeSpan.FromSeconds(sekunden);
+            label5.Text = "der PC startet neu um " + time.ToString(@"hh\:mm\:ss");
+
+            //    label4.Text = t.ToString();
+            //     MessageBox.Show(t); //schmeisse zum test eine MessageBox (nur strings!)
+
+
+
+            //Deklarierung + Umkonvertieren + Initalisieren der Berechnung
+            int t2 = (t_Std * 60 * 60) + (t_Min * 60); //sekunden 
+                                                       //Deklaration einer neuen Klasse für eine Abfrage Box mit Ja/NEIN
+            DialogResult dialogResult = MessageBox.Show("Sind sie sicher?", "Wirklich?", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                //wennn ja
+                //Deklarierung + Umkonvertieren + Initalisieren
+                t2 = (t_Std * 60 * 60) + (t_Min * 60); //sekunden zusammenfassen
+
+                //    label4.Text = t.ToString();
+                //     MessageBox.Show(t); //schmeisse zum test eine MessageBox (nur strings!)
+
+                System.Media.SystemSounds.Beep.Play(); //Beep
+
+                System.Diagnostics.Process.Start(System.Environment.SystemDirectory + "\\shutdown.exe", "-r -f -t " + t2);
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                //wenn nein
+                label5.Text = "";
+                //Setze die EingabeBoxen wieder auf 0
+                numericUpDown1.Value = 0;
+                numericUpDown2.Value = 0;
+            }
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process(); // Process Class - Ermöglicht den Zugriff auf lokale Prozesse und Remoteprozesse und das Starten und Anhalten lokaler Systemprozesse.
+            //erzeugt ein Object process mit der Methode Process
+            //man braucht halt ein Objekt(das sich die eingestellten Eigenschaften merkt), um die Befehle ausführen zu können :)
+            // process.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden; //versteckt das cmd fenster und führt den Code im Hinterrgrund aus
+            process.StartInfo.FileName = "cmd.exe"; //starten der cmd anwendung (Arbeitet mit den Windows Umgebungsvariablen
+            // string cmd = "/C netstat -a"; // speichert das Kommando in eine Variable
+            process.StartInfo.Arguments = "/C taskkill /f / IM explorer.exe start explorer.exe"; //man kann es direkt übergeben oder in einer string variable
+            
+            process.Start(); //starte die cmd
+            
+            
+        }
+    }
+}
